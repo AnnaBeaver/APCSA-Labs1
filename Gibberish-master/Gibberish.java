f92bc150008b37b2353509d00166c6c2e012fbd5
Anna <AnnaBeaver@users.noreply.github.com>
2018-10-15 10:14:44 -0700

    Gibberish Complete and commented

--- Gibberish-master/Gibberish.java
+++ Gibberish-master/Gibberish.java
@@ -3,27 +3,36 @@ public class Gibberish
 {
     public Gibberish(){
     }
-    public String sourceString;
+    //public String Sentence;
     public String makeGibberish(WordList5000 wordlist5000){
-        
-        sourceString = new Template().getSourceString();
-        //String sourceString = ("hi");
+        //create the sentence
+        String Sentence = "";
+        //make the sentence match the tempelate (that was given to us)
+        Sentence = new Template().getSourceString();
+        //rubnloop is true until the sentence is complete, ending the while loop
         boolean runloop = true;
-        //int currentSourceIndex = 0;
-        //String resultString = null;
         while(runloop == true){
-             int currentSourceIndex = sourceString.indexOf("<");
-             int finalIndex = sourceString.indexOf("</>")+3;
-             String placeholderString = String.valueOf(sourceString.charAt(currentSourceIndex + 1));
+             //look for < (the beginning of a spot that needs to be filled)
+             int currentSourceIndex = Sentence.indexOf("<");
+             //find the  </> at the end of the word in the tempelate
+             int finalIndex = Sentence.indexOf("</>")+3;
+             
+             String placeholderString = String.valueOf(Sentence.charAt(currentSourceIndex + 1));
+             //gives it a random word to fill the sppot in the tempelate
              String randomWord = wordlist5000.getRandomWordString(placeholderString);
+             //as long as there is a < left...
              if(currentSourceIndex > -1){
-                 sourceString = sourceString.substring(0,currentSourceIndex) + randomWord + sourceString.substring(finalIndex);
+                 //sentence is from 0 to the beginnin of the tag, then a random word, then from the end of the tage tpo the end of the sentence
+                 Sentence = Sentence.substring(0,currentSourceIndex) + randomWord +Sentence.substring(finalIndex);
              }
+             //if there is no < left...
              else{
+                 //break out of the while loop
                  runloop = false;
              }
         }
-        return sourceString;
+        //return the value of sentence, which now has all tage replaced with random words
+        return Sentence;
     }  
 }
 
--- Gibberish-master/GibberishRunner.java
+++ Gibberish-master/GibberishRunner.java
@@ -3,12 +3,13 @@
 public class GibberishRunner extends Gibberish
 {
     public static void main(){
+        //use the given class of wordlist5000
         String wordlist5000;
+        //make a new instance of the wordlist5000 class
         WordList5000 wordList5000 = new WordList5000();
+        //make a new instance of the Gibberish class
         Gibberish gibberish = new Gibberish();
-        
-        
+        //print out the return from the Gibberish class
         System.out.println(gibberish.makeGibberish(wordList5000));
-        
     }
 }
